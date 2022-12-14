745ec3251102a4ad22c969acccc17c2dc004c0d8
onion1007 <85293570+onion0905@users.noreply.github.com>
2022-12-14 23:24:21 +0800

    add hangman template

---
+++ hangman.py
@@ -0,0 +1,15 @@
+import random
+import string
+# You can add any words you like
+# Or from this place : 'https://www.randomlists.com/data/words.json'
+words = ["alike","basic","abandon","embarrassed","alert","circumstance","aim","object","fundamental","Physics","unusual","aboard","original","attractive","research","expression","abrupt","absent","absorbed","absorbing","abstracted","absurd","abundant","massive","accept","acceptable","accessible","accidental","account","accuracy"]
+
+def get_valid_word(words):
+    return
+
+
+def hangman():
+    return    
+
+if __name__ == '__main__':
+    hangman()
